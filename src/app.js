96220bb0dd2b4c64fc542ddec590556c1bfbdcc8
unknown <joelqm@hotmail.com>
2023-05-30 16:36:31 -0600

    error 3

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {pool} from './db.js'
+import {PORT} from './config.js'
 
 const app = express();
 
 app.get('/', (req,res) =>{
-    res.send('Bienvenido al servidor otra vez 2')
+    res.send('Bienvenido al servidor otra vez 3')
 })
 
 app.get('/ping', async (req,res) =>{
@@ -21,5 +22,5 @@ app.get('/create', async (req,res) =>{
 
 
 //app.listen(3306);
-//app.listen(3000);
-//console.log('server en el puerto 3000')
+app.listen(PORT);
+console.log('server en el puerto 3000')
