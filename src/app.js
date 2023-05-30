59168f7ab1ad4e133d5cbf1eb61d988aa033548e
unknown <joelqm@hotmail.com>
2023-05-30 16:51:16 -0600

    error 4

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@ import {PORT} from './config.js'
 
 const app = express();
 
+const puerto = PORT;
+
 app.get('/', (req,res) =>{
-    res.send('Bienvenido al servidor otra vez 3')
+    //res.send('Bienvenido al servidor otra vez 3 en el puerto ',PORT)
+    res.send(`Bienvenido al servidor otra vez 3 en el puerto ${puerto}`)
 })
 
 app.get('/ping', async (req,res) =>{
@@ -23,4 +26,4 @@ app.get('/create', async (req,res) =>{
 
 //app.listen(3306);
 app.listen(PORT);
-console.log('server en el puerto 3000')
+console.log(`server en el puerto ${puerto}`)
