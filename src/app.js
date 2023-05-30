6fc6bd313138ce0ca10f6aff8f729e2a9080faee
unknown <joelqm@hotmail.com>
2023-05-30 15:46:54 -0600

    agregando cambio

--- src/app.js
+++ src/app.js
@@ -21,5 +21,5 @@ app.get('/create', async (req,res) =>{
 
 
 //app.listen(3306);
-app.listen(3000);
-console.log('server en el puerto 3000')
+//app.listen(3000);
+//console.log('server en el puerto 3000')
